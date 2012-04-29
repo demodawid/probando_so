e3995e510ae1a3a3a5b001da737a5451da03a75e
demodawid <demodawid@gmail.com>
2012-04-29 15:56:29 -0700

    Esto es un test!

---
+++ elMejorProgramaDeLaHistoria.c
@@ -0,0 +1,14 @@
+/*
+ * elMejorProgramaDeLaHistoria.c
+ *
+ *  Created on: Apr 29, 2012
+ *      Author: demian
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	puts("malloc-eame esta");
+	return 0;
+}
